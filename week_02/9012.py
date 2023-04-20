9e8f5beeaaaf3c109e95aae01711fe1154a91658
kelvin3476 <winrain03226@gmail.com>
2023-04-20 15:11:08 +0900

    4/20

---
+++ week_02/9012.py
@@ -0,0 +1,21 @@
+import sys
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    command = sys.stdin.readline()
+    stack = []
+
+    for i in command:
+        if i == '(':
+            stack.append('(')
+        elif i == ')':
+            if len(stack) == 0:
+                stack.append(')')
+                break
+            else:
+                stack.pop()
+
+    if len(stack) != 0:
+        print('NO')
+    else:
+        print('YES')
---
+++ week_02/heap.py
@@ -0,0 +1,12 @@
+#source code: https://www.daleseo.com/python-heapq/
+
+import heapq
+
+nums = [4, 1, 7, 3, 8, 5]
+heap = []
+
+for num in nums:
+    heapq.heappush(heap, (-num, num))
+
+while heap:
+    print(heapq.heappop(heap)[1])
